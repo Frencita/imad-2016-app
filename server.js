2307801413f30074714038d52ff4c1d23eabc8d6
Frencita <francita03@gmail.com>
2016-09-23 19:31:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/article-two', function (req, res) {
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article three will be served and requested here');
+  res.send('Article  will be served and requested here');
 });
 
 app.get('/ui/style.css', function (req, res) {
