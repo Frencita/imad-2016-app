25d57fb8d2ca186669bd504dce2bfa2b73966bf5
Frencita <francita03@gmail.com>
2016-09-23 22:37:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element=document.getElementById('maintext');
 Element.iorhtml='new value';
  
  var img=document.getElementById("madi");
- img.onclick=function()
+ img.onclick = function()
  {
  img.style; marginleft="100px"};
